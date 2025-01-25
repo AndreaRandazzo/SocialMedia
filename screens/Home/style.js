9ed2df41da0f1a19dd623911d68e4d30cd35af34
AndreaRandazzo <153932512+AndreaRandazzo@users.noreply.github.com>
2025-01-25 18:59:25 +0100

    Adds Stack Navigation

---
+++ screens/Home/style.js
@@ -0,0 +1,49 @@
+import {StyleSheet} from 'react-native';
+import {getFontFamily} from '../../assets/fonts/helper';
+import {
+  horizontalScale,
+  scaleFontSize,
+  verticalScale,
+} from '../../assets/styles/scaling';
+
+const style = StyleSheet.create({
+  header: {
+    marginLeft: horizontalScale(27),
+    marginRight: horizontalScale(17),
+    marginTop: verticalScale(30),
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  messageIcon: {
+    padding: horizontalScale(14),
+    borderRadius: horizontalScale(100),
+    backgroundColor: '#F9FAFB',
+  },
+  messageNumberContainer: {
+    backgroundColor: '#F35BAC',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    width: horizontalScale(10),
+    height: horizontalScale(10),
+    borderRadius: horizontalScale(12),
+    position: 'absolute',
+    right: horizontalScale(10),
+    top: verticalScale(10),
+  },
+  messageNumber: {
+    color: 'white',
+    fontSize: scaleFontSize(8),
+    fontFamily: getFontFamily('Inter', 600),
+  },
+  userStoryContainer: {
+    marginTop: verticalScale(20),
+    marginHorizontal: horizontalScale(28),
+  },
+  userPost: {
+    marginHorizontal: horizontalScale(24),
+  },
+});
+
+export default style;
