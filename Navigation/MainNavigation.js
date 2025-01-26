1bfc59f140d3b0b8cf74d4ad4f4ff581194ed66f
AndreaRandazzo <153932512+AndreaRandazzo@users.noreply.github.com>
2025-01-26 12:40:12 +0100

    Adds navigation menu and first part of Profile screen

--- Navigation/MainNavigation.js
+++ Navigation/MainNavigation.js
@@ -2,16 +2,24 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {Routes} from './Route';
 import Home from '../screens/Home/Home';
 import Profile from '../screens/Profile/Profile';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const MainMenuNavigation = () => {
+  return (
+    <Drawer.Navigator screenOptions={{headerShown: false}}>
+      <Drawer.Screen name={Routes.Home} component={Home} />
+      <Drawer.Screen name={Routes.Profile} component={Profile} />
+    </Drawer.Navigator>
+  );
+};
 
 const MainNavigation = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{headerShown: false}}
-      initialRouteName={Routes.Home}>
-      <Stack.Screen name={Routes.Home} component={Home} />
-      <Stack.Screen name={Routes.Profile} component={Profile} />
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name={'Drawer'} component={MainMenuNavigation} />
     </Stack.Navigator>
   );
 };
